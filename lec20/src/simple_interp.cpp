59c5975302509fb1ddf819488d34b8f0aba54898
Jesse A. Tov <jesse.tov@gmail.com>
2018-12-06 11:50:25 -0600

    Rearranged to make more sense.

--- lec20/src/simple_interp.cpp
+++ lec20/src/simple_interp.cpp
@@ -2,49 +2,18 @@
 #include <iostream>
 #include <memory>
 
+// Interface for expressions.
 class Expr
 {
 public:
+    // Evaluates this expression.
     virtual int evaluate() const = 0;
 };
 
+// We will typically hold onto expressions via shared pointers.
 using expr_ptr = std::shared_ptr<Expr>;
 
-class Lit_expr : public Expr
-{
-public:
-    explicit Lit_expr(int);
-
-    int evaluate() const override;
-
-private:
-    int value_;
-};
-
-class Sub_expr : public Expr
-{
-public:
-    Sub_expr(expr_ptr, expr_ptr);
-
-    int evaluate() const override;
-
-private:
-    expr_ptr left_, right_;
-    // INVARIANT: left_ and right_ are not null
-};
-
-class Mul_expr : public Expr
-{
-public:
-    Mul_expr(expr_ptr, expr_ptr);
-
-    int evaluate() const override;
-
-private:
-    expr_ptr left_, right_;
-    // INVARIANT: left_ and right_ are not null
-};
-
+// Factory functions for constructing each kind of expression.
 expr_ptr mk_lit_expr(int);
 expr_ptr mk_sub_expr(expr_ptr, expr_ptr);
 expr_ptr mk_mul_expr(expr_ptr, expr_ptr);
@@ -57,6 +26,22 @@ int main()
     std::cout << e->evaluate() << "\n";
 }
 
+///
+/// DETAILS BELOW
+///
+
+// Represents an integer literal expression.
+class Lit_expr : public Expr
+{
+public:
+    explicit Lit_expr(int);
+
+    int evaluate() const override;
+
+private:
+    int value_;
+};
+
 Lit_expr::Lit_expr(int value)
         : value_(value)
 { }
@@ -65,6 +50,24 @@ int Lit_expr::evaluate() const
 {
     return value_;
 }
+
+expr_ptr mk_lit_expr(int value)
+{
+    return std::make_shared<Lit_expr>(value);
+}
+
+// Represents an subtraction expression.
+class Sub_expr : public Expr
+{
+public:
+    Sub_expr(expr_ptr, expr_ptr);
+
+    int evaluate() const override;
+
+private:
+    expr_ptr left_, right_;
+    // INVARIANT: left_ and right_ are not null
+};
 
 Sub_expr::Sub_expr(expr_ptr left, expr_ptr right)
         : left_(std::move(left))
@@ -78,6 +81,24 @@ int Sub_expr::evaluate() const
     return left_->evaluate() - right_->evaluate();
 }
 
+expr_ptr mk_sub_expr(expr_ptr left, expr_ptr right)
+{
+    return std::make_shared<Sub_expr>(std::move(left), std::move(right));
+}
+
+// Represents a multiplication expression.
+class Mul_expr : public Expr
+{
+public:
+    Mul_expr(expr_ptr, expr_ptr);
+
+    int evaluate() const override;
+
+private:
+    expr_ptr left_, right_;
+    // INVARIANT: left_ and right_ are not null
+};
+
 Mul_expr::Mul_expr(expr_ptr left, expr_ptr right)
         : left_(std::move(left))
         , right_(std::move(right))
@@ -88,16 +109,6 @@ Mul_expr::Mul_expr(expr_ptr left, expr_ptr right)
 int Mul_expr::evaluate() const
 {
     return left_->evaluate() * right_->evaluate();
-}
-
-expr_ptr mk_lit_expr(int value)
-{
-    return std::make_shared<Lit_expr>(value);
-}
-
-expr_ptr mk_sub_expr(expr_ptr left, expr_ptr right)
-{
-    return std::make_shared<Sub_expr>(std::move(left), std::move(right));
 }
 
 expr_ptr mk_mul_expr(expr_ptr left, expr_ptr right)
